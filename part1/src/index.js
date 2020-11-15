3b1e5a839759eb2cf709c9cebf5e2d734cb11b9c
yolopunk <cynosurech@gmail.com>
2020-11-15 22:25:54 +0800

    feat(part1): introduction to react

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  React.createElement(App, null),
-  document.getElementById('root')
-)
+const Hello = (props) => {
+  return (
+    <p>Hello {props.name}, you are {props.age} years old</p>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part name="Fundamentals of React" exercises={10} />
+      <Part name="Using props to pass data" exercises={7} />
+      <Part name="State of a component" exercises={14} />
+    </div>
+  )
+}
+
+const Total = () => {
+  return (
+    <p>Number of exercises {10 + 7 + 14}</p>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  const name = 'Peter'
+  const age = 10
+
+  const course = 'Half Stack application development'
+
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <h1>Greeting</h1>
+      <Hello name="Mayo" age={26 + 10} />
+      <Hello name={name} age={age} />
+
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
