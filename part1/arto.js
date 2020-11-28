e6105c0597020b3ac0054722afec070cc6de7515
yolopunk <cynosurech@gmail.com>
2020-11-28 18:21:10 +0800

    feat(part1): refactor

---
+++ part1/arto.js
@@ -0,0 +1,9 @@
+const arto = {
+  name: 'Arto Hellas',
+  greet: function() {
+    console.log('hello, my name is ' + this.name)
+  }
+}
+
+arto.greet()
+setTimeout(arto.greet.bind(arto))
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+/* 1.1
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
-    <p>Hello {props.name}, you are {props.age} years old</p>
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
   )
 }
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>
+  return <h1>{props.course.name}</h1>
 }
 const Part = (props) => {
   return (
@@ -40,8 +46,24 @@ const App = () => {
   const name = 'Peter'
   const age = 10
 
-  const course = 'Half Stack application development'
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+      name: "Fundamentals of React",
+      exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      }
+    ]
+  }
+  
   return (
     <div>
       <p>Hello world, it is {now.toString()}</p>
@@ -58,5 +80,70 @@ const App = () => {
     </div>
   )
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
+*/
+
+
+/* 1.2
+const Display = ({ counter }) => <div>{counter}</div>
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter - 1)
+
+  console.log('render...')
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text='plus' />
+      <Button handleClick={setToZero} text="zero" /> 
+      <Button handleClick={decreaseByOne} text='minus' /> 
+    </div>
+  )
+}
+*/
+
+const App = () => {
+  const [clicks, setClicks] = useState({ left: 0, right: 0 })
+  const handleLeftClick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1
+    }
+
+    setClicks(newClicks)
+  }
+
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    }
+
+    setClicks(newClicks)
+  }
+
+  return (
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
