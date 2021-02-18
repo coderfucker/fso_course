d883072328243c62739fa8f0c172972ec034ec55
yolopunk <cynosurech@gmail.com>
2021-02-18 18:38:46 +0800

    feat(part2): exercise 2.1-2.5

---
+++ 2.1-2.5/src/components/Course.js
@@ -0,0 +1,42 @@
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const total = course.parts.reduce((acc, cur) => {
+    return acc + cur.exercises
+  }, 0)
+  return(
+    <p>total of {total} exercises</p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => <Part key={part.name} part={part} />)}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+export default Course
